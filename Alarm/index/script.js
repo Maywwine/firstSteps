a7099db57dcf15e04d77012d65f644a5f72a5ff4
May <147344175+Maywwine@users.noreply.github.com>
2024-03-15 11:20:58 +0100

    time
    time and options added

--- Alarm/index/script.js
+++ Alarm/index/script.js
@@ -1,6 +1,28 @@
 const selectMenu = document.querySelectorAll('select');
+const actualTime = document.querySelector('.actual-time');
+
 for(let i = 23 ; i >=0 ; i--){
    i = i < 10 ? "0"+ i : i;
     let option = `<option value="${i}">${i}</option>`
-    console.log(option);
+    selectMenu[0].firstElementChild.insertAdjacentHTML('afterend', option);
 }
+
+for(let i = 59; i >= 0 ; i-- ){
+    let option = `<option value=${i}>${i}</option>`
+    i = i< 10 ? '0'+ i : i;
+    selectMenu[1].firstElementChild.insertAdjacentHTML('afterend', option);
+}
+
+setInterval( ()=> {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+     h = h < 10 ? '0'+ h : h;
+     m = m < 10 ? '0' + m : m;
+     s = s <10 ? '0' + s : s;
+    
+    actualTime.innerHTML = `${h}:${m}:${s}`;
+
+} , 1000);
